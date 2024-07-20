a8764d5fcc8298d5655197f1e0c735e79be1cb14
hamze ghanbari <ghanbarih243@gmail.com>
2024-07-20 00:33:21 -0700

    update data generator

--- src/app/shared/utils/data-generator.ts
+++ src/app/shared/utils/data-generator.ts
@@ -1,3 +1,4 @@
+import { isObject, isString, isNumber, isBoolean, isNull, isDate } from "./utils";
 
 const configData = {
     countString: 20,
@@ -124,33 +125,7 @@ function randomprimitiveData(type: any) {
         return null;
 }
 
-let isString = (value: any, strict: boolean = false): value is String => {
-    let result = value == 'string' || value == 'String' || typeof value == 'string';
-    if (!strict)
-        return result;
-    else
-        return result && !isNumber(value) && !isBoolean(value) && !isDate(value);
-}
 
-let isNumber = (value: any): value is Number => {
-    return value == 'number' || value == 'Number' || typeof value == 'number';
-}
-
-let isBoolean = (value: any): value is boolean => {
-    return value == 'boolean' || value == 'Boolean' || typeof value == 'boolean';
-}
-
-let isDate = (value: any): value is Date => {
-    return value == 'date' || value == 'Date' || value instanceof Date;
-}
-
-let isNull = (value: any): value is null => {
-    return value === null || value == 'null';
-}
-
-let isObject = (value: any): value is Object => {
-    return typeof value === 'object' && value !== null && !Array.isArray(value) && value instanceof Date == false;
-}
 
 function generateData(object: any) {
     let result: any = {};

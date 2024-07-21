8b37a909b1bfb0ed6f789648eb44e2404a6ab26f
hamze ghanbari <ghanbarih243@gmail.com>
2024-07-20 23:12:45 -0700

    update dismiss event directive

--- src/app/shared/directives/dismiss-event.directive.ts
+++ src/app/shared/directives/dismiss-event.directive.ts
@@ -1,11 +1,23 @@
-import { Directive, HostListener, Input } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { DestroyRef, Directive, HostListener, Inject, InjectionToken, Input, PLATFORM_ID, inject } from '@angular/core';
 
 @Directive({
   selector: '[appDismissEvent]',
   standalone: true
 })
 export class DismissEventDirective {
-  constructor() {}
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(PLATFORM_ID) private paltFormId: InjectionToken<Object>
+  ) {
+    inject(DestroyRef).onDestroy(() => {
+      if(isPlatformBrowser(this.paltFormId)){
+        this.document.removeEventListener('copy', this.copy);
+        this.document.removeEventListener('paste', this.paste);
+        this.document.removeEventListener('cut', this.cut);
+      }
+    });
+  }
 
   @HostListener('copy', ['$event']) copy(event: Event) {
     event.preventDefault();

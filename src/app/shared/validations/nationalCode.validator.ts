ef7242d0f154477f32435bbe13c20fca061078ec
hamze ghanbari <ghanbarih243@gmail.com>
2024-07-27 16:05:19 -0700

    update nationaleCode validator

--- src/app/shared/validations/nationalCode.validator.ts
+++ src/app/shared/validations/nationalCode.validator.ts
@@ -1,7 +1,7 @@
 import { ValidatorFn, AbstractControl, ValidationErrors } from "@angular/forms";
 
-export function nationalCodeValidator(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
+// export function nationalCodeValidator(): ValidatorFn {
+     export function nationalCodeValidator(control: AbstractControl): ValidationErrors | null  {
         let nationalCode = control.value.trim();
         let bannedArray = ['0000000000', '1111111111', '2222222222', '3333333333', '4444444444', '5555555555', '6666666666', '7777777777', '8888888888', '9999999999'];
 
@@ -35,4 +35,4 @@ export function nationalCodeValidator(): ValidatorFn {
 
         }
     }
-}
+// }

ab67ac68af2d21caac401a007c3142b79e52ffe2
hamze ghanbari <ghanbarih243@gmail.com>
2024-08-03 18:16:44 +0430

    update config file

--- src/app/core/constants/config.ts
+++ src/app/core/constants/config.ts
@@ -3,6 +3,7 @@ import { indexType } from "@core/models/types";
 export const APP_CONFIG: indexType = {
     timeOut : 10000,
     expireCache: 30000,
+    fileSize: 2000000, // 2mb
     accessTokenKey: 'access_token',
     refreshTokenKey: 'refresh_token',
     expireTokenKey: 'expire_token',

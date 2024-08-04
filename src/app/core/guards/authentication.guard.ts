160b35ba584beece96026dc20743835614691de1
hamze ghanbari <ghanbarih243@gmail.com>
2024-08-05 00:52:31 +0430

    update guards

--- src/app/core/guards/authentication.guard.ts
+++ src/app/core/guards/authentication.guard.ts
@@ -1,8 +1,11 @@
+import { inject } from '@angular/core';
 import { CanActivateFn, Route, Router } from '@angular/router';
 import { UserService } from '@core/services/http-service/user.service';
 import { getInstance } from '@core/utils/injector';
 
 export const authenticationGuard: CanActivateFn = (route, state) => {
-  const instance = getInstance([UserService, Router]);
-  return instance.userservice.userAccessToken() ? true : instance.router.navigate(['/login']);
+  // const instance = getInstance([UserService, Router]);
+  const userService : UserService = inject(UserService);
+  const router : Router = inject(Router);
+  return userService.userAccessToken() ? true : router.navigate(['/login']);
 };
--- src/app/core/guards/token.guard.ts
+++ src/app/core/guards/token.guard.ts
@@ -1,8 +1,12 @@
+import { inject } from '@angular/core';
 import { CanActivateFn, Route, Router } from '@angular/router';
+import { PwaService } from '@core/services/helper-service/pwa.service';
 import { UserService } from '@core/services/http-service/user.service';
 import { getInstance } from '@core/utils/injector';
 
 export const tokenGuard: CanActivateFn = (route, state) => {
-  const instance = getInstance([UserService, Router]);
-  return !instance.userservice.userAccessToken() ? true : instance.router.navigate(['/dashboard']);
+  // const instance = getInstance(UserService, Router, PwaService);
+  const userService : UserService = inject(UserService);
+  const router : Router = inject(Router);
+  return !userService.userAccessToken() ? true : router.navigate(['/dashboard']);
 };
